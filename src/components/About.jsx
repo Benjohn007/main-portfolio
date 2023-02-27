f6bf348d9551c409e83e0ce135299842bcbdd3ab
Benjohn007 <johanben330@gmail.com>
2023-02-27 02:28:30 +0100

    text  corrections

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,20 +10,20 @@ const About = () => {
                 <p className='text-4xl font-bold inline border-b-4
                 border-gray-500'>About</p>
             </div>
-            <p className=' text-base'>I am an agile software developer with relevant years of work experience who is highly skilled in
-                producing top-notch, high-quality work. My ability to quickly learn and absorb new information
+            <p className=' text-base'>I am an agile software developer who is highly skilled in
+                producing top-notch and high-quality work with relevant years of work experience. 
+                My ability to quickly learn and absorb new information
                 allows me to effectively develop and construct cutting-edge electronic document systems, and
                 apply that knowledge to real-world problem-solving scenarios with great proficiency. I possess a
-                unique capability of adapting to new and rapidly-evolving technical environments in a timely
+                unique capability of adapting to new and rapidly evolving technical environment in a timely
                 manner. My programming expertise is exemplified by my exceptional proficiency in the .NET
                 framework, specifically utilizing C# language for ASP.NET MVC and ASP.NET Core
                 development.
             </p>
             <br/>
             <p className=' text-base mt-4 lg:mt-4'>
-                A Versatile software engineer and designer with a passion for 
-                creating high-quality, engaging and entertaining user experience.
-                I'm intrigued by design, strategy, technology and better conversations.
+               I am passionate about creating designs with high-quality, engaging and entertaining 
+               user experience with awesome strategy, technology and better conversations.
             </p>
         </div>
     </div>
